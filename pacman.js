fe544080bac98d3579fedb484d46aa6d26a03b29
DacPhai <nguyendacphais@gmail.com>
2026-03-04 17:44:08 +0700

    pacman_map2.js

--- pacman.js
+++ pacman.js
@@ -23,22 +23,22 @@ const tileMap = [
     "XXXXXXXXXXXXXXXXXXX",
     "X        X        X",
     "X XX XXX X XXX XX X",
+    "X X      P      X X",
+    "X X XXX XXX XXX X X",
     "X                 X",
-    "X XX X XXXXX X XX X",
-    "X    X       X    X",
-    "XXXX XXXX XXXX XXXX",
-    "OOOX X       X XOOO",
-    "XXXX X XXrXX X XXXX",
-    "O       bpo       O",
-    "XXXX X XXXXX X XXXX",
-    "OOOX X       X XOOO",
-    "XXXX X XXXXX X XXXX",
-    "X        X        X",
+    "X XXX XXXXXXX XXX X",
+    "X X      X      X X",
+    "X X XXXX X XXXX X X",
+    "X      b r p o    X",
+    "X X XXXX X XXXX X X",
+    "X X      X      X X",
+    "X XXX XXXXXXX XXX X",
+    "X                 X",
+    "X X XXX XXX XXX X X",
+    "X X      X      X X",
     "X XX XXX X XXX XX X",
-    "X  X     P     X  X",
-    "XX X X XXXXX X X XX",
-    "X    X   X   X    X",
-    "X XXXXXX X XXXXXX X",
+    "X        X        X",
+    "X XXXXXXXXXXXXXXX X",
     "X                 X",
     "XXXXXXXXXXXXXXXXXXX" 
 ];
@@ -349,3 +349,4 @@ class Block {
         this.y = this.startY;
     }
 };
+
